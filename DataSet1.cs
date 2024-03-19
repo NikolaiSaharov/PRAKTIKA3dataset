8423a3359f19b7cd33f35ec2c06fa0d2c4f5ca80
NikolaiSaharov <isip_n.v.saharov@mpt.ru>
2024-03-19 10:04:06 +0300

    PRAKTIKA3dataset

---
+++ DataSet1.cs
@@ -0,0 +1,15 @@
+﻿namespace PRAKTIKA3
+{
+
+
+    partial class DataSet1
+    {
+    }
+}
+
+namespace PRAKTIKA3.DataSet1TableAdapters {
+    
+    
+    public partial class FlightsTableAdapter {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PRAKTIKA3.DataSet1TableAdapters;
+namespace PRAKTIKA3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PassengersTableAdapter passengers= new PassengersTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            TicketGrid.ItemsSource = passengers.GetDataBy();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            TicketGrid.Columns[0].Visibility = Visibility.Collapsed;
+            TicketGrid.Columns[5].Visibility = Visibility.Collapsed;
+        }
+    }
+}
